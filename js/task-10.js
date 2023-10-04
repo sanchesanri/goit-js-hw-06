381f8d4ce394c3387ede7c09b3f08805efd0b75e
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2023-10-04 12:58:28 +0300

    oy

--- js/task-10.js
+++ js/task-10.js
@@ -1,23 +1,63 @@
-/*
-*Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
-<div id="controls">
-  <input type="number" min="1" max="100" step="1" />
-  <button type="button" data-create>Create</button>
-  <button type="button" data-destroy>Destroy</button>
-</div>
-<div id="boxes"></div>
-Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+const inputRef = document.querySelector('input');
+const btnCreateRef = document.querySelector('button[data-create]');
+const btnDestroyRef = document.querySelector('button[data-destroy]');
+const boxMarkup = document.querySelector('#boxes');
 
-Размеры самого первого <div> - 30px на 30px.
-Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+btnCreateRef.addEventListener('click', createMarkup);
+btnDestroyRef.addEventListener('click', removeMarkup);
+
+function createMarkup(ev) {
+    boxMarkup.innerHTML = '';
+    createAnotherBoxes(inputRef.value);
+
+    //   createBoxes(inputRef.value)
 }
-Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
-*/
+
+
+
+
+function createAnotherBoxes(amount) {
+    const quantityBoxes = Number(amount);
+
+    const arr = new Array(quantityBoxes);
+
+    const fillNewArr = arr
+        .fill()
+        .map(
+            (item,index) =>
+                (item = `<div class="coloredSquare" style="width: ${30 + index * 10}px; height: ${30 + index * 10}px; background: ${getRandomHexColor()}"></div>`),
+        )
+        .join('');
+
+    boxMarkup.insertAdjacentHTML('beforeend',fillNewArr)
+}
+
+// function createBoxes(amount) {
+//     const number = Number(amount);
+//     const makeMarkup = [];
+//     let numberForTag = 20;
+
+//     for (let i = 0; i < number; i += 1) {
+//         numberForTag += 10;
+//         const createDiv = document.createElement('div');
+//         createDiv.classList.add('coloredSquare');
+//         createDiv.style.width += numberForTag + 'px';
+//         createDiv.style.height += numberForTag + 'px';
+//         createDiv.style.background = getRandomHexColor();
+//         makeMarkup.push(createDiv);
+//     }
+//     console.log(makeMarkup);
+
+//     return boxMarkup.append(...makeMarkup);
+// }
+
+function removeMarkup() {
+    boxMarkup.innerHTML = '';
+    inputRef.value = '';
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
