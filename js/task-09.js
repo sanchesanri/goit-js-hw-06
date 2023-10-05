8916a2ef2bdd73faf535364346e5afd7bfe766a9
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2023-10-05 15:09:39 +0300

    ghange 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,20 +1,21 @@
 const widgetRef = document.querySelector('.widget');
 const textSpanRef = document.querySelector('.color');
 
+widgetRef.addEventListener('click', onButtonChangeColorClick);
 
-widgetRef.addEventListener('click', onButtonChangeColorClick)
+function onButtonChangeColorClick(ev) {
+    ev.preventDefault();
 
-function onButtonChangeColorClick (ev) {
-  ev.preventDefault()
+    const randomHexColor = getRandomHexColor();
 
-  if (ev.target.nodeName === 'BUTTON') {
-    document.body.style.background = getRandomHexColor();textSpanRef.textContent = getRandomHexColor();
-  }
+    if (ev.target.nodeName === 'BUTTON') {
+        document.body.style.background = randomHexColor;
+        textSpanRef.textContent = randomHexColor;
+    }
 }
 
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
